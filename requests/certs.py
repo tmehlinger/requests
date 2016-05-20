711f454fd49680ca8b55387b7cf8d80c7eb3e485
Travis Mehlinger <tmehlinger@gmail.com>
2016-05-20 14:51:33 -0500

    use a plugin to override the CA cert bundle path

--- requests/certs.py
+++ requests/certs.py
@@ -12,14 +12,21 @@ environment, you can change the definition of where() to return a separately
 packaged CA bundle.
 """
 import os.path
+import pkg_resources
 
-try:
-    from certifi import where
-except ImportError:
-    def where():
-        """Return the preferred certificate bundle."""
-        # vendored bundle inside Requests
-        return os.path.join(os.path.dirname(__file__), 'cacert.pem')
+
+for entry_point in pkg_resources.iter_entry_points(
+        group='requests.ca_bundle', name=None):
+    where = entry_point.load()
+    break
+else:
+    try:
+        from certifi import where
+    except ImportError:
+        def where():
+            """Return the preferred certificate bundle."""
+            # vendored bundle inside Requests
+            return os.path.join(os.path.dirname(__file__), 'cacert.pem')
 
 if __name__ == '__main__':
     print(where())
